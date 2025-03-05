08c90a79bd792ad06b4fb1c7fd897e27266ff565
ErNikhilTiwari <102575456+ErNikhilTiwari@users.noreply.github.com>
2025-03-05 10:35:16 +0530

    [Nikhil] ADD new methods in unit converter

--- UnitConverter.java
+++ UnitConverter.java
@@ -13,7 +13,33 @@ public class UnitConverter {
         double yards2feet = 3;
         return yards * yards2feet;
     }
+    public static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
 
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        return pounds * pounds2kilograms;
+    }
+
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        return kilograms * kilograms2pounds;
+    }
+
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        return gallons * gallons2liters;
+    }
+
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        return liters * liters2gallons;
+    }
     public static double convertFeetToYards(double feet) {
         double feet2yards = 0.333333;
         return feet * feet2yards;
